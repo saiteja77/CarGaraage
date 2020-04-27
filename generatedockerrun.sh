4baa81b3a5c9e82d7d34bf09d50609d9736745bb
Sai Reddy <sai.reddy@target.com>
2020-04-27 17:35:22 -0500

    replace tag

--- generatedockerrun.sh
+++ generatedockerrun.sh
@@ -1,12 +1,2 @@
-echo -e "{
-  \"AWSEBDockerrunVersion\": \"1\",
-  \"Image\": {
-    \"Name\": \"bitbyte01/cargaraage:${CIRCLE_BUILD_NUM}\",
-    \"Update\": \"true\"
-  },
-  \"Ports\": [
-    {
-      \"ContainerPort\": 8080
-    }
-  ]
-}" > Dockerrun.aws.json
+sed -i -- 's@<TAG>@'"$CIRCLE_BUILD_NUM"'@' Dockerrun.aws.json
+rm Dockerrun.aws.json--
